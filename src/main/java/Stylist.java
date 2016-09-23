9764090de423b34af96fa3d8954b5177bc38519c
Georgina Van Dort <georginavandort@gmail.com>
2016-09-23 10:00:05 -0700

    add stylist class with getters and save function. All passing testing

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -4,8 +4,58 @@ import java.util.List;
 import org.sql2o.*;
 
 
-// public class {
-//
-//
-//
-// }
+public class Stylist {
+  private String name;
+  private int id;
+
+  public Stylist (String name) {
+    this.name = name;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public static List<Stylist> all() {
+    String sql = "SELECT id, name FROM stylists";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Stylist.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherStylist) {
+    if (!(otherStylist instanceof Stylist)) {
+      return false;
+    } else {
+      Stylist newStylist = (Stylist) otherStylist;
+      return this.getName().equals(newStylist.getName());
+    }
+  }
+
+  public void save() {
+  try(Connection con = DB.sql2o.open()) {
+    String sql = "INSERT INTO stylists (name) VALUES (:name)";
+    this.id = (int) con.createQuery(sql, true)
+      .addParameter("name", this.name)
+      .executeUpdate()
+      .getKey();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -1,24 +1,52 @@
-// import org.sql2o.*;
-// import org.junit.*;
-// import static org.junit.Assert.*;
-// import java.util.Arrays;
-// import java.util.List;
-// import java.util.ArrayList;
-//
-//
-// public class StylistTest {
-//
-//   @Before
-//     public void setUp() {
-//       DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
-//     }
-//
-//   @After
-//   public void tearDown() {
-//     try(Connection con = DB.sql2o.open()) {
-//       String deleteStylistQuery = "DELETE FROM stylists *;";
-//       String deleteClientQuery = "DELETE FROM clients *;";
-//       con.createQuery(deleteStylistQuery).executeUpdate();
-//       con.createQuery(deleteClientQuery).executeUpdate();
-//     }
-//   }
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class StylistTest {
+
+  @Before
+    public void setUp() {
+      DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
+    }
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteStylistQuery = "DELETE FROM stylists *;";
+      String deleteClientQuery = "DELETE FROM clients *;";
+      con.createQuery(deleteStylistQuery).executeUpdate();
+      con.createQuery(deleteClientQuery).executeUpdate();
+    }
+  }
+
+  @Test
+  public void Stylist_instantiatesCorrectly_true() {
+    Stylist testStylist = new Stylist("David Bowie");
+    assertEquals(true, testStylist instanceof Stylist);
+  }
+
+  @Test
+  public void Stylist_instantiatesWithName_String() {
+    Stylist myStylist = new Stylist("David Bowie");
+    assertEquals("David Bowie", myStylist.getName());
+  }
+
+  @Test
+   public void equals_returnsTrueIfNamesAretheSame() {
+     Stylist firstStylist = new Stylist("David Bowie");
+     Stylist secondStylist = new Stylist("David Bowie");
+     assertTrue(firstStylist.equals(secondStylist));
+   }
+
+   @Test
+    public void save_savesIntoDatabase_true() {
+      Stylist myStylist = new Stylist("David Bowie");
+      myStylist.save();
+      assertTrue(Stylist.all().get(0).equals(myStylist));
+    }
+
+}

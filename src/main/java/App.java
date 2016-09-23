9d75c471f949982a94de4f96b5fa0f6114fccd87
Georgina Van Dort <georginavandort@gmail.com>
2016-09-23 11:08:47 -0700

    add basic vtl files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,10 +11,32 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
-    // get("/", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/index.vtl" );
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl" );
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/stylists", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      Stylist newStylist = new Stylist(name);
+      newStylist.save();
+      model.put("stylist", newStylist);
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+
+
+
+
+
+
+
+
+
+
   }
 }

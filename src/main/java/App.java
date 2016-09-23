2350600483be37784465e3347da682d1ab581da8
Georgina Van Dort <georginavandort@gmail.com>
2016-09-23 11:53:08 -0700

    provide stylist page, client list, and add new client functions

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -28,9 +28,31 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/stylists/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+      List<Client> client = stylist.getClients();
+      model.put("client", client);
+      model.put("stylist", stylist);
+      model.put("template", "templates/stylist.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
+    post("/clients", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.find(Integer.parseInt(request.queryParams("stylist_id")));
 
+      String name = request.queryParams("name");
+      String phone = request.queryParams("phone");
+      Client newClient = new Client(name, phone, stylist.getId());
+      newClient.save();
 
+      model.put("stylist", stylist);
+      model.put("client", newClient);
+      model.put("template", "templates/success.vtl");
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
 
 

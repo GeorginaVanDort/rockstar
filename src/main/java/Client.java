afd889641158801fbff07fe645b33aadadb5febb
Georgina Van Dort <georginavandort@gmail.com>
2016-09-23 09:43:08 -0700

    add save, update and delete functions to client class

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -20,6 +20,10 @@ public class Client {
     return name;
   }
 
+  public int getId() {
+    return id;
+  }
+
   public String getPhone() {
     return phone;
   }
@@ -27,6 +31,69 @@ public class Client {
   public int getStylistId() {
     return stylist_id;
   }
+
+  public static List<Client> all() {
+      String sql = "SELECT id, name, phone, stylist_id FROM clients";
+      try(Connection con = DB.sql2o.open()) {
+        return con.createQuery(sql).executeAndFetch(Client.class);
+      }
+    }
+
+  @Override
+  public boolean equals(Object otherClient) {
+    if (!(otherClient instanceof Client)) {
+      return false;
+    } else {
+      Client newClient = (Client) otherClient;
+      return this.getName().equals(newClient.getName())
+      && this.getId() == newClient.getId()
+      && this.getPhone().equals(newClient.getPhone())
+      && this.getStylistId() == newClient.getStylistId();
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO clients(name, phone, stylist_id) VALUES(:name, :phone, :stylist_id)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .addParameter("phone", this.phone)
+        .addParameter("stylist_id", this.stylist_id)
+        .executeUpdate()
+        .getKey();
+      }
+    }
+
+  public static Client find(int id) {
+  try(Connection con = DB.sql2o.open()) {
+    String sql = "SELECT * FROM clients where id=:id";
+    Client client = con.createQuery(sql)
+      .addParameter("id", id)
+      .executeAndFetchFirst(Client.class);
+      return client;
+    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+    String sql = "DELETE FROM clients WHERE id = :id;";
+    con.createQuery(sql)
+      .addParameter("id", id)
+      .executeUpdate();
+    }
+  }
+
+  public void update(String name, String phone) {
+    try(Connection con = DB.sql2o.open()) {
+    String sql = "UPDATE clients SET name = :name, phone = :phone WHERE id = :id";
+    con.createQuery(sql)
+      .addParameter("name", name)
+      .addParameter("phone", phone)
+      .addParameter("id", id)
+      .executeUpdate();
+    }
+  }
+
 
 
 
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -31,19 +31,80 @@ public class ClientTest {
 
     @Test
     public void Client_instantiatesWithName_String() {
-      Client myClient = new Client("Fred", "503-111-1111", 1);
-      assertEquals("Fred", myClient.getName());
+      Client testClient = new Client("Fred", "503-111-1111", 1);
+      assertEquals("Fred", testClient.getName());
     }
 
     @Test
     public void Client_instantiatesWithPhone_String() {
-      Client myClient = new Client("Fred", "503-111-1111", 1);
-      assertEquals("503-111-1111", myClient.getPhone());
+      Client testClient = new Client("Fred", "503-111-1111", 1);
+      assertEquals("503-111-1111", testClient.getPhone());
     }
 
     @Test
     public void Client_instantiatesWithStylistId_Int() {
-      Client myClient = new Client("Fred", "503-111-1111", 1);
-      assertEquals(1, myClient.getStylistId());
+      Client testClient = new Client("Fred", "503-111-1111", 1);
+      assertEquals(1, testClient.getStylistId());
     }
+
+    @Test
+    public void Client_instantiatesWithId_True() {
+      Client testClient = new Client("Fred", "503-111-1111", 1);
+      testClient.save();
+      assertTrue(testClient.getId() > 0);
+    }
+
+    @Test
+    public void equals_returnsTrueIfClientsAretheSame_True() {
+      Client oneClient = new Client("Fred", "503-111-1111", 1);
+      oneClient.save();
+      Client twoClient = new Client("Fred", "503-111-1111", 1);
+      twoClient.save();
+      assertEquals(true, Client.all().get(0).equals(oneClient));
+      assertEquals(true, Client.all().get(1).equals(twoClient));
+    }
+
+    @Test
+    public void save_returnsTrueIfClientsAretheSame_True() {
+      Client testClient = new Client("Fred", "503-111-1111", 1);
+      testClient.save();
+      assertTrue(Client.all().get(0).equals(testClient));
+    }
+
+    @Test
+    public void save_assignsIdToObject_True() {
+      Client testClient = new Client("Fred", "503-111-1111", 1);
+      testClient.save();
+      Client savedClient = Client.all().get(0);
+      assertEquals(testClient.getId(), savedClient.getId());
+    }
+
+    @Test
+    public void find_returnsClientWithSameId_secondClient() {
+      Client firstClient = new Client("Fred", "503-111-1111", 1);
+      firstClient.save();
+      Client secondClient = new Client("Fred", "503-111-1111", 1);
+      secondClient.save();
+      assertEquals(Client.find(secondClient.getId()), secondClient);
+    }
+
+    @Test
+    public void update_updatesClientDescription_true() {
+      Client testClient = new Client("Fred", "503-111-1111", 1);
+      testClient.save();
+      testClient.update("Jim", "503-111-2222");
+      assertEquals("Jim", Client.find(testClient.getId()).getName());
+    }
+
+    @Test
+    public void delete_deletesClient_true() {
+      Client testClient = new Client("Fred", "503-111-1111", 1);
+      testClient.save();
+      int testClientId = testClient.getId();
+      testClient.delete();
+      assertEquals(null, Client.find(testClientId));
+    }
+
+
+
 }
